a5905fc6a9f1af058d0f142d3ddece83a94ec6e6
Adil Soomro <sad.bird009@gmail.com>
2011-12-14 12:12:13 +0500

    Source code :)

---
+++ TabSample/src/app/tabsample/ArrowsActivity.java
@@ -0,0 +1,16 @@
+package app.tabsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class ArrowsActivity extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arrowspage);      
+    }
+}
